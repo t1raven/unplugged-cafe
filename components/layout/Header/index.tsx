f617cc64cf77dd82f30b063fea6e3f8d0c57d06a
t1raven <eziz@naver.com>
2026-09-09 15:10:52 +0900

    instagram toggle menu

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState  } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useParams, useRouter, usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
@@ -98,6 +98,36 @@ export default function Header() {
     };
   }, [pathname]);
 
+
+  const [active, setActive] = useState<boolean>(false);
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleInstagram = () => setActive((prev) => !prev);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setActive(false);
+      }
+    };
+
+    const handleScroll = () => {
+      setActive(false);
+    };
+
+    if(active) {
+      document.addEventListener('mousedown', handleOutsideClick);
+      window.addEventListener('scroll', handleScroll, { passive: true });
+    }
+    
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [active]);
+
+
   //ThemeToggle
   const { theme, setTheme } = useTheme();
   const [ mounted, setMounted ] = useState(false);
@@ -124,9 +154,39 @@ export default function Header() {
           </Link>
         </div>
         <div className="gnb-btn">
-          <Link href="https://www.instagram.com/unplugged.lounge/" target="_blank">
+          {/*<Link href="https://www.instagram.com/unplugged.lounge/" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320.3 205C256.8 204.8 205.2 256.2 205 319.7C204.8 383.2 256.2 434.8 319.7 435C383.2 435.2 434.8 383.8 435 320.3C435.2 256.8 383.8 205.2 320.3 205zM319.7 245.4C360.9 245.2 394.4 278.5 394.6 319.7C394.8 360.9 361.5 394.4 320.3 394.6C279.1 394.8 245.6 361.5 245.4 320.3C245.2 279.1 278.5 245.6 319.7 245.4zM413.1 200.3C413.1 185.5 425.1 173.5 439.9 173.5C454.7 173.5 466.7 185.5 466.7 200.3C466.7 215.1 454.7 227.1 439.9 227.1C425.1 227.1 413.1 215.1 413.1 200.3zM542.8 227.5C541.1 191.6 532.9 159.8 506.6 133.6C480.4 107.4 448.6 99.2 412.7 97.4C375.7 95.3 264.8 95.3 227.8 97.4C192 99.1 160.2 107.3 133.9 133.5C107.6 159.7 99.5 191.5 97.7 227.4C95.6 264.4 95.6 375.3 97.7 412.3C99.4 448.2 107.6 480 133.9 506.2C160.2 532.4 191.9 540.6 227.8 542.4C264.8 544.5 375.7 544.5 412.7 542.4C448.6 540.7 480.4 532.5 506.6 506.2C532.8 480 541 448.2 542.8 412.3C544.9 375.3 544.9 264.5 542.8 227.5zM495 452C487.2 471.6 472.1 486.7 452.4 494.6C422.9 506.3 352.9 503.6 320.3 503.6C287.7 503.6 217.6 506.2 188.2 494.6C168.6 486.8 153.5 471.7 145.6 452C133.9 422.5 136.6 352.5 136.6 319.9C136.6 287.3 134 217.2 145.6 187.8C153.4 168.2 168.5 153.1 188.2 145.2C217.7 133.5 287.7 136.2 320.3 136.2C352.9 136.2 423 133.6 452.4 145.2C472 153 487.1 168.1 495 187.8C506.7 217.3 504 287.3 504 319.9C504 352.5 506.7 422.6 495 452z"/></svg>
-          </Link>
+          </Link>*/}
+          <div className={`sns_btns ${active ? 'active' : ''}`} ref={dropdownRef}>
+            <button type="button" onClick={toggleInstagram}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320.3 205C256.8 204.8 205.2 256.2 205 319.7C204.8 383.2 256.2 434.8 319.7 435C383.2 435.2 434.8 383.8 435 320.3C435.2 256.8 383.8 205.2 320.3 205zM319.7 245.4C360.9 245.2 394.4 278.5 394.6 319.7C394.8 360.9 361.5 394.4 320.3 394.6C279.1 394.8 245.6 361.5 245.4 320.3C245.2 279.1 278.5 245.6 319.7 245.4zM413.1 200.3C413.1 185.5 425.1 173.5 439.9 173.5C454.7 173.5 466.7 185.5 466.7 200.3C466.7 215.1 454.7 227.1 439.9 227.1C425.1 227.1 413.1 215.1 413.1 200.3zM542.8 227.5C541.1 191.6 532.9 159.8 506.6 133.6C480.4 107.4 448.6 99.2 412.7 97.4C375.7 95.3 264.8 95.3 227.8 97.4C192 99.1 160.2 107.3 133.9 133.5C107.6 159.7 99.5 191.5 97.7 227.4C95.6 264.4 95.6 375.3 97.7 412.3C99.4 448.2 107.6 480 133.9 506.2C160.2 532.4 191.9 540.6 227.8 542.4C264.8 544.5 375.7 544.5 412.7 542.4C448.6 540.7 480.4 532.5 506.6 506.2C532.8 480 541 448.2 542.8 412.3C544.9 375.3 544.9 264.5 542.8 227.5zM495 452C487.2 471.6 472.1 486.7 452.4 494.6C422.9 506.3 352.9 503.6 320.3 503.6C287.7 503.6 217.6 506.2 188.2 494.6C168.6 486.8 153.5 471.7 145.6 452C133.9 422.5 136.6 352.5 136.6 319.9C136.6 287.3 134 217.2 145.6 187.8C153.4 168.2 168.5 153.1 188.2 145.2C217.7 133.5 287.7 136.2 320.3 136.2C352.9 136.2 423 133.6 452.4 145.2C472 153 487.1 168.1 495 187.8C506.7 217.3 504 287.3 504 319.9C504 352.5 506.7 422.6 495 452z"/></svg>
+            </button>
+            <div className="subMenu">
+              <ul>
+                <li>
+                  <Link href="https://www.instagram.com/unplugged.lounge/" target="_blank">
+                     <i className="material-symbols-rounded icon" translate="no">music_note_2</i>
+                    언플러그드 라운지
+                    <i className="material-symbols-rounded outlink" translate="no">arrow_outward</i>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://www.instagram.com/cafeunplugged.seogyo/" target="_blank">
+                    <i className="material-symbols-rounded icon" translate="no">coffee</i>
+                    서교음악다방
+                    <i className="material-symbols-rounded outlink" translate="no">arrow_outward</i>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://www.instagram.com/unplugged.lounge.zip/" target="_blank">
+                    <i className="material-symbols-rounded icon" translate="no">news</i>
+                    언플러그드 라운지 매거진
+                    <i className="material-symbols-rounded outlink" translate="no">arrow_outward</i>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
           <button type="button" onClick={() => setTheme((theme === 'dark' ? 'light' : 'dark'))}>
             <span className="material-symbols-rounded">{theme === 'dark' ? 'light_mode' : 'dark_mode'}</span>
           </button>
